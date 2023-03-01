5e8b9cecb8114ee3c4c1386c3f9382e0c63bb583
Ant0C <cafeo97@gmail.com>
2023-03-01 15:08:11 +0100

    FEAT project and components

---
+++ vite-comics/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppHeader from './components/HelloWorld.vue'
+import AppMain from './components/HelloWorld.vue'
+import AppFooter from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    AppFooter,
+    AppHeader,
+    AppMain,
+  }
+}
+</script>
+
+<template></template>
+
+<style></style>
